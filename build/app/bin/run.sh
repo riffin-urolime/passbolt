50093f386aed048491e5e2838a70d57e209278e8
root <root@ip-172-31-20-251.us-east-2.compute.internal>
2018-04-02 07:56:02 +0000

    Passbolt updated

---
+++ build/app/bin/run.sh
@@ -0,0 +1,85 @@
+#!/usr/bin/env bash
+
+if [ ! -e /var/www/passbolt/index.php ]; then
+    echo "Couldn't find any files for passbolt, downloading new files"
+    mkdir -p /var/www/passbolt
+    cd /var/www/passbolt
+    curl -L https://github.com/passbolt/passbolt_api/archive/v1.3.0.tar.gz -o /home/www-data/passbolt.tar.gz
+    echo "extracting..."
+    tar -xzf /home/www-data/passbolt.tar.gz -C /var/www/passbolt --strip-components=1
+    chown -R www-data /var/www
+    
+    cp -a /var/www/passbolt/app/Config/app.php.default /var/www/passbolt/app/Config/app.php
+    cp -a /var/www/passbolt/app/Config/core.php.default /var/www/passbolt/app/Config/core.php
+    # cp -a /var/www/passbolt/app/webroot/js/app/config/config.json.default /var/www/passbolt/app/webroot/js/app/config/config.json
+    chown -R www-data /var/www
+
+    # gpg
+    GPG_SERVER_KEY_FINGERPRINT=`gpg -n --with-fingerprint /home/www-data/gpg_server_key_public.key | awk -v FS="=" '/Key fingerprint =/{print $2}' | sed 's/[ ]*//g'`
+    /var/www/passbolt/app/Console/cake passbolt app_config write GPG.serverKey.fingerprint $GPG_SERVER_KEY_FINGERPRINT
+    /var/www/passbolt/app/Console/cake passbolt app_config write GPG.serverKey.public /home/www-data/gpg_server_key_public.key
+    /var/www/passbolt/app/Console/cake passbolt app_config write GPG.serverKey.private /home/www-data/gpg_server_key_private.key
+
+    # cake alwys writes strings...
+    #/var/www/passbolt/app/Console/cake passbolt app_config write App.ssl.force false
+    #sed -i  "/'force' => true,/c\'force' => false," /var/www/passbolt/app/Config/app.php
+
+    chown www-data:www-data /home/www-data/gpg_server_key_public.key
+    chown www-data:www-data /home/www-data/gpg_server_key_private.key
+    chown -R www-data /var/www
+
+    # overwrite the core configuration
+    /var/www/passbolt/app/Console/cake passbolt core_config gen-cipher-seed
+    /var/www/passbolt/app/Console/cake passbolt core_config gen-security-salt
+    /var/www/passbolt/app/Console/cake passbolt core_config write App.fullBaseUrl https://${HOST_NAME}
+    chown -R www-data /var/www
+    # overwrite the database configuration
+    # @TODO based on the cake task DbConfigTask implement a task to manipulate the dabase configuration
+    #/var/www/passbolt/app/Console/cake passbolt db_config ${MYSQL_HOST} ${MYSQL_USERNAME} ${MYSQL_PASSWORD} ${MYSQL_DATABASE}
+
+    DATABASE_CONF=/var/www/passbolt/app/Config/database.php
+    # Set configuration in file
+    cat > $DATABASE_CONF << EOL
+        <?php
+        class DATABASE_CONFIG {
+            public \$default = array(
+                'datasource' => 'Database/Mysql',
+                'persistent' => false,
+                'host' => '${MYSQL_HOST}',
+                'login' => '${MYSQL_USERNAME}',
+                'password' => '${MYSQL_PASSWORD}',
+                'database' => '${MYSQL_DATABASE}',
+                'prefix' => '',
+                'encoding' => 'utf8',
+            );
+        };
+EOL
+    
+    # email config
+    sed -i "/\s*public \$default = array(/,/\s*);/s/'transport' => 'Smtp',/'transport' => '$APP_EMAIL_TRANSPORT',/" /var/www/passbolt/app/Config/email.php
+    sed -i "/\s*public \$default = array(/,/\s*);/s/'from' => array('contact@passbolt.com' => 'Passbolt'),/'from' => array('$APP_EMAIL_FROM_ADDRESS' => '$APP_EMAIL_FROM_NAME'),/" /var/www/passbolt/app/Config/email.php
+    sed -i "/\s*public \$default = array(/,/\s*);/s/'host' => 'smtp.mandrillapp.com',/'host' => '$APP_EMAIL_HOST',/" /var/www/passbolt/app/Config/email.php
+    sed -i "/\s*public \$default = array(/,/\s*);/s/'port' => 587,/'port' => $APP_EMAIL_PORT,/" /var/www/passbolt/app/Config/email.php
+    sed -i "/\s*public \$default = array(/,/\s*);/s/'timeout' => 30,/'timeout' => $APP_EMAIL_TIMEOUT,/" /var/www/passbolt/app/Config/email.php
+    sed -i "/\s*public \$default = array(/,/\s*);/s/'username' => '',/'username' => '$APP_EMAIL_USERNAME',/" /var/www/passbolt/app/Config/email.php
+    sed -i "/\s*public \$default = array(/,/\s*);/s/'password' => '',/'password' => '$APP_EMAIL_PASSWORD',/" /var/www/passbolt/app/Config/email.php
+    # the interaction between bash an sed is really weird, i had to triple escape the first tab
+    sed -i "/\s*'password' => '$APP_EMAIL_PASSWORD',/a \\\t\t'tls' => true," /var/www/passbolt/app/Config/email.php
+
+    # depending on the users system, the db container can take some time to start up, 10 seconds should give it enough time to be ready to handle connections
+    echo "Waiting 10 seconds for mysql container to finish initializing"
+    sleep 10
+    chown -R www-data /var/www
+    echo "Installing"
+    su -s /bin/bash -c "/var/www/passbolt/app/Console/cake install --no-admin" www-data
+    echo "Registering admin with credentials - email: ${ADMIN_EMAIL} - firstname: ${ADMIN_FIRSTNAME} - lastname: ${ADMIN_LASTNAME}"
+    echo "########################################################################################################################"
+    echo "########################################################################################################################"
+    su -s /bin/bash -c "/var/www/passbolt/app/Console/cake passbolt register_user -u  ${ADMIN_EMAIL} -f ${ADMIN_FIRSTNAME} -l ${ADMIN_LASTNAME} -r admin" www-data
+    echo "########################################################################################################################"
+    echo "########################################################################################################################"
+    echo "We are all set. Have fun with Passbolt !"
+
+fi
+echo "Starting supervisor"
+/usr/bin/supervisord -c /etc/supervisor/conf.d/supervisor.conf
---
+++ build/app/debian.Dockerfile
@@ -0,0 +1,117 @@
+FROM debian:jessie-slim
+
+ARG LOG_ERROR
+ARG LOG_ACCESS
+ARG MEMORY_LIMIT
+ARG POST_MAXSIZE
+ARG UPLOAD_MAX_FILESIZE
+ARG DATE_TIMEZONE
+
+
+RUN apt-get update && \
+    apt-get install -y --no-install-recommends \
+    # persistent & runtime deps. \
+    ca-certificates curl wget libpcre3 librecode0 libsqlite3-0 libxml2 \
+    # php 
+    php5-json php5-cli php5-fpm php5-common \
+    php5-curl php5-gd php5-mcrypt \
+    php5-mysql php5-xsl php5-intl \
+    # gpg dependencies
+    libgpgme11-dev \
+    # cache
+    php5-memcached memcached\
+    # pear
+    php-pear \
+    # supervisor
+    supervisor
+
+# Configure the user www-data environment
+RUN mkdir /home/www-data/ \
+    && chown www-data:www-data /home/www-data/ \
+    && usermod -d /home/www-data www-data
+
+RUN mkdir /var/www
+
+# Configure node and install grunt
+# On debian they choose to rename node in nodejs, some tools try to access nodejs by using the commande noe.
+#RUN ln -s /usr/bin/nodejs /usr/bin/node \
+#    # install grunt
+#    && npm install -g grunt-cli
+
+## gnupg
+# phpize / pecl dependencies 
+RUN apt-get install -y --no-install-recommends \
+    autoconf file g++ gcc libc-dev make pkg-config re2c php5-dev
+
+RUN pecl install gnupg \
+    && echo "extension=gnupg.so;" > /etc/php5/mods-available/gnupg.ini \
+    && ln -s /etc/php5/mods-available/gnupg.ini /etc/php5/fpm/conf.d/20-gnupg.ini \
+    && ln -s /etc/php5/mods-available/gnupg.ini /etc/php5/cli/conf.d/20-gnupg.ini \
+    # configure the user www-data env to work with gnupg \
+    && mkdir /home/www-data/.gnupg \
+    && chown www-data:www-data /home/www-data/.gnupg \
+    && chmod 0777 /home/www-data/.gnupg
+
+# gpg keys
+COPY ./keys/gpg_server_key_public.key /home/www-data/gpg_server_key_public.key
+COPY ./keys/gpg_server_key_private.key /home/www-data/gpg_server_key_private.key
+
+COPY ./keys /root/.gnupg
+COPY ./keys /home/www-data/.gnupg
+RUN chown -R www-data /home/www-data
+
+RUN apt-get remove -y \
+    autoconf file g++ gcc libc-dev make pkg-config re2c php5-dev
+
+## php config
+# config php5-fpm to use port 9000 instead of a unix socket
+RUN sed -i "/listen =/c\listen = \[\:\:\]\:9000" /etc/php5/fpm/pool.d/www.conf
+
+# reroute php access and error log for display in docker log
+# we dont configure this for cli, since whoever is logged into a container to use the cli will see the output anyway
+RUN if ( $LOG_ERROR ); then  sed -i "/error_log =/c\error_log = \/proc\/self\/fd\/2" /etc/php5/fpm/php-fpm.conf; fi
+RUN if ( $LOG_ACCESS ); then  sed -i "/;access.log =/c\access.log = \/proc\/self\/fd\/2" /etc/php5/fpm/pool.d/www.conf; fi
+
+# file size configs
+RUN sed -i "/memory_limit =/c\memory_limit = $MEMORY_LIMIT" /etc/php5/fpm/php.ini
+RUN sed -i "/memory_limit =/c\memory_limit = $MEMORY_LIMIT" /etc/php5/cli/php.ini
+RUN sed -i "/post_max_size =/c\post_max_size = $POST_MAXSIZE" /etc/php5/fpm/php.ini
+RUN sed -i "/post_max_size =/c\post_max_size = $POST_MAXSIZE" /etc/php5/cli/php.ini
+RUN sed -i "/upload_max_filesize =/c\upload_max_filesize = $UPLOAD_MAX_FILESIZE" /etc/php5/fpm/php.ini
+RUN sed -i "/upload_max_filesize =/c\upload_max_filesize = $UPLOAD_MAX_FILESIZE" /etc/php5/cli/php.ini
+
+# date timezone
+RUN sed -i "/;date.timezone =/c\date.timezone = $DATE_TIMEZONE" /etc/php5/fpm/php.ini
+RUN sed -i "/;date.timezone =/c\date.timezone = $DATE_TIMEZONE" /etc/php5/cli/php.ini
+
+# create folder for php pid files
+RUN mkdir /run/php
+
+# install composer
+COPY ./bin/install-composer-debian.sh /tmp/install-composer.sh
+RUN chmod +x /tmp/install-composer.sh 
+RUN /tmp/install-composer.sh
+RUN mv /composer.phar /usr/local/bin/composer
+
+# cron for mail
+RUN apt-get install -y --no-install-recommends cron
+COPY ./config/mailer-cron /etc/cron.d/mailer-cron
+RUN chmod 0644 /etc/cron.d/mailer-cron
+RUN touch /var/log/passbolt.log
+RUN crontab /etc/cron.d/mailer-cron
+
+# supervisor
+COPY ./config/supervisor.conf /etc/supervisor/conf.d/supervisor.conf
+RUN chmod 0644 /etc/supervisor/conf.d/supervisor.conf
+
+EXPOSE 9000
+
+# cleanup
+RUN apt-get clean && rm -rf /var/lib/apt/lists/*
+RUN apt-get autoremove -y
+RUN rm -rf /tmp/*
+
+COPY ./bin/run.sh /run.sh
+RUN chmod +x /run.sh
+
+CMD ["/run.sh"]
